acc5a1ce8ad115175f4a32d1d497e4d864a0f76b
AleksandrsC <mirrorfrequency@gmail.com>
2022-03-24 10:59:06 +0200

    Problem 1 class

---
+++ src/com/accenture/bootcamp/task4/ColourFun.java
@@ -0,0 +1,18 @@
+package com.accenture.bootcamp.task4;
+
+/**
+        *  1.declare and initialize String array with 7arbitrary colors.
+        *  2. Create new list "colors" (ArrayList) of type String
+        *  3. Copy/add all elements from String array to colors list
+        *  4. Remove 3rd element from colors list
+        *  5. Add new color at the end of the colors list
+        *  6. Add new color at the beginning of the colors list
+        *  7. Loop through colors list and output all values (use for each loop)
+        *  8. Create new list (colorsSublist) which will be sublist of the colors list (will contain elements from
+        *     index 2 to index 5)
+        *     (hint: take a look at List interface and find a method which you can use to achieve this)
+        *  9. Use for loop to output each element of colorsSublist list
+ */
+public class ColourFun {
+
+}
